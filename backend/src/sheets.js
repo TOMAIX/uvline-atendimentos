0162aff4887c0342288f17ad2cc6c6cb2423a6b6
TOMAIX <32846004+TOMAIX@users.noreply.github.com>
2025-01-29 09:40:06 -0300

    Adicionando package.json e atualizando sheets.js

--- backend/src/sheets.js
+++ backend/src/sheets.js
@@ -1,25 +1,26 @@
 const { google } = require('googleapis');
 
-const credentials = require('../uvline-atendimentos-189918a5b7e5.json');
+// Usar as credenciais da variável de ambiente
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 const SPREADSHEET_ID = '1sZumguCoCt_OnlqvqaWMPe-qzezbv3fy_YCFCLg3WZ8';
 
 const auth = new google.auth.GoogleAuth({
- credentials,
- scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  credentials,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets']
 });
 
 const sheets = google.sheets({ version: 'v4', auth });
 
 async function appendRow(store, transcription) {
- const date = new Date().toLocaleString('pt-BR');
- const values = [[date, store, transcription]];
+  const date = new Date().toLocaleString('pt-BR');
+  const values = [[date, store, transcription]];
 
- await sheets.spreadsheets.values.append({
-   spreadsheetId: SPREADSHEET_ID,
-   range: 'Sheet1!A:C',
-   valueInputOption: 'USER_ENTERED',
-   resource: { values }
- });
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: 'Sheet1!A:C',
+    valueInputOption: 'USER_ENTERED',
+    resource: { values }
+  });
 }
 
 module.exports = { appendRow };
